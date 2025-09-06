6d64dfd0a9d308262d627188dff318a676c464c5
Vova Revenko <vova@revenko.org>
2025-09-06 23:08:53 +0400

    feat(settings): init

---
+++ src/settings/page-settings.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div>{{ $t('settings.PageSettings.title') }}</div>
+  </div>
+</template>
---
+++ src/settings/routes.ts
@@ -0,0 +1,24 @@
+import type { RouteRecordInfo, RouteRecordRaw } from 'vue-router'
+
+export interface SettingsRoutes {
+  settings: RouteRecordInfo<
+    'settings',
+    '/settings',
+    Record<never, never>,
+    Record<never, never>
+  >
+}
+
+export const settingsRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('~/shared/layouts/layout-empty.vue'),
+    children: [
+      {
+        path: 'settings',
+        name: 'settings',
+        component: () => import('./page-settings.vue'),
+      },
+    ],
+  },
+]
